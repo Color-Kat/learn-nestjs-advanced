9251ffef91475f7ac49a77d4d6c072b3578f44d8
Color-Kat <katik7979@gmail.com>
2024-07-23 16:28:19 +0300

    connect tot typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,31 @@ import { AppService } from "./app.service";
 import { SongsModule } from "./songs/songs.module";
 import { LoggerMiddleware } from "./common/middleware/logger/logger.middleware";
 import { SongsController } from "./songs/songs.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { DataSource } from "typeorm";
 
 @Module({
-    imports: [SongsModule],
+    imports: [
+        SongsModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            database: 'spotify_clone',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'root',
+            autoLoadEntities: true,
+            synchronize: true
+        })
+    ],
     controllers: [AppController],
     providers: [AppService]
 })
 export class AppModule implements NestModule {
+    constructor(private dataSource: DataSource) {
+        console.log(dataSource.driver.database)
+    }
+
     configure(consumer: MiddlewareConsumer): any {
         // consumer.apply(LoggerMiddleware).forRoutes('songs');
         // consumer.apply(LoggerMiddleware).forRoutes({path: 'songs', method: RequestMethod.POST});
