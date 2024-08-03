ae533157d057054e7b84fb47feabdb73c56b3082
Color-Kat <katik7979@gmail.com>
2024-08-03 21:33:11 +0300

    increase speed of hmr by webpack

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-import { SeedService } from "@/seed/seed.service";
 import { ConfigService } from "@nestjs/config";
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,8 +16,12 @@ async function bootstrap() {
   // const seedService = app.get(SeedService);
   // await seedService.seed();
 
-
   await app.listen(configService.get<number>('port'));
+
+  if(module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 
 bootstrap();
