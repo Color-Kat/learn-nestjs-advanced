31aa64b6a09a0043e101e7b94bd8acb2f98efe8e
Color-Kat <katik7979@gmail.com>
2024-08-05 17:00:07 +0300

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,37 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
 
-  const configService = app.get(ConfigService);
+    const configService = app.get(ConfigService);
 
+    // Data seeding
+    // const seedService = app.get(SeedService);
+    // await seedService.seed();
 
-  // Data seeding
-  // const seedService = app.get(SeedService);
-  // await seedService.seed();
+    // Swagger setup
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Spotify Clone')
+        .setDescription('The spotify clone API documentation by @ColorKat')
+        .setVersion('1.0')
+        .build();
 
-  await app.listen(configService.get<number>('port'));
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
 
-  if(module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+    await app.listen(configService.get<number>("port"));
+
+    // Configure webpack hmr
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
 }
 
 bootstrap();
