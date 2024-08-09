b8122a5d7a0bb70f34cc744106bbd94d7454d64b
Color-Kat <katik7979@gmail.com>
2024-08-09 11:16:22 +0300

    jest mock functions

--- Jest-basics/mock-function.spec.js
+++ Jest-basics/mock-function.spec.js
@@ -7,5 +7,24 @@ describe('Mock Function examples', () => {
         expect(mockFunction()).toBe(777);
         expect(mockFunction.mock.calls.length).toBe(2);
         expect(mockFunction).toHaveBeenCalled();
-    })
+    });
+
+    it('should create a mock function with an argument', () => {
+        const mockCreateSong = jest.fn((createSongDto) => ({...createSongDto, id: 1}));
+        expect(mockCreateSong({title: 'title'})).toEqual({title: 'title', id: 1});
+    });
+
+    it('should create a mock function with an implementation', () => {
+        const mockCreateSong = jest.fn();
+        mockCreateSong.mockImplementation((createSongDto) => ({...createSongDto, id: 1}));
+
+        expect(mockCreateSong({title: 'title'})).toEqual({title: 'title', id: 1});
+    });
+
+    it('should create a mock function with promise', () => {
+        const fetchSongs = jest.fn();
+        fetchSongs.mockResolvedValue({id: 1});
+
+        expect(fetchSongs()).resolves.toEqual({id: 1});
+    });
 });
